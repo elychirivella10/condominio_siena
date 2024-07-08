034de6797cf0dff95df67a6434a0aaa6fc441ca6
elychirivella10 <elychirivella10@gmail.com>
2024-07-08 12:29:00 -0400

    variables globales

--- src/helpers/variablesGoblales.js
+++ src/helpers/variablesGoblales.js
@@ -1,4 +1,4 @@
 //export const rutaAxios = "https://minaguas.com/";
 //export const rutaAxios = "https://censoapi.sapi.gob.ve/api/"
-//xport const rutaAxios = "https://perfect-nature-production.up.railway.app/api/"
-export const rutaAxios = "http://localhost:5000/api/"
+export const rutaAxios = "https://perfect-nature-production.up.railway.app/api/"
+//export const rutaAxios = "http://localhost:5000/api/"
